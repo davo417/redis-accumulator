c854e34f6fe71f964ecfdcd054a310d422816ca8
davidcode43w <davidgutierrezmenendez@gmail.com>
2023-01-30 04:43:27 -0500

    redis script api change

--- tests/db_insert.py
+++ tests/db_insert.py
@@ -1,4 +1,3 @@
-
 import clickhouse_connect
 from pathlib import Path
 from uuid import uuid4
@@ -17,42 +16,40 @@ r.flushall()
 
 def load_script(path):
     with open(path) as script:
-        hash = r.script_load(script.read())
+        hash = r.register_script(script.read())
     return hash
 
 scripts = {Path(n).stem: load_script(n) for n in glob("src/lua/*.lua")}
 
-def run(name, nkeys=0, *args):
-    return r.evalsha(scripts[name], nkeys, *args)
+# def run(name, nkeys=0, *args):
+#     return r.evalsha(scripts[name], nkeys, *args)
 
-schema = run('schema', 5, 'test', 'user_id', 'username', 'active', 'rate', 'UInt32', 'String', 'Bool', 'Float64')
+schema = scripts['schema'](keys=['test', 'user_id', 'username', 'active', 'rate'], args=['UInt32', 'String', 'Bool', 'Float64'])
 # print("Schema: ", r.hgetall(f"schema:{schema}"))
 
-run('swap', 0, str(uuid4()))
+scripts['swap'](args=[str(uuid4())])
 chunk_id = r.get("chunk:id:current")
 # print(f"Chunk id: {chunk_id}")
 
-run('insert', 4, 'user_id', 'username', 'active', 'rate', 1, 'user1', 'false', 5.0)
-run('insert', 4, 'user_id', 'username', 'active', 'rate', 2, 'user2', 'true', 4.5)
+scripts['insert'](keys=['user_id', 'username', 'active', 'rate'], args=[1, 'user1', 'false', 5.0])
+scripts['insert'](keys=['user_id', 'username', 'active', 'rate'], args=[2, 'user2', 'true', 4.5])
 # print("Chunk len: ", r.get(f"chunk:{chunk_id}:len"))
 
-run('swap', 0, str(uuid4()))
+scripts['swap'](args=[str(uuid4())])
 
-data = json.loads(run('export', 4, 'user_id', 'username', 'active', 'rate', 'test'))
+data = json.loads(scripts['export'](keys=['user_id', 'username', 'active', 'rate'], args=['test']))
 c.insert('test', data=list(data['data'].values()), column_names=list(data['data'].keys()), column_oriented=True)
 
-run('clean')
+scripts['clean']()
+scripts['insert'](keys=['user_id', 'username', 'active', 'rate'], args=[3, 'user3', 'false', 3.1])
+scripts['insert'](keys=['user_id', 'username', 'active', 'rate'], args=[4, 'user4', 'true', 1.6])
+scripts['insert'](keys=['user_id', 'username', 'active', 'rate'], args=[5, 'user5', 'false', 0.3])
+scripts['insert'](keys=['user_id', 'username', 'active', 'rate'], args=[6, 'user6', 'true', 2.9])
 
-run('insert', 4, 'user_id', 'username', 'active', 'rate', 3, 'user3', 'false', 3.1)
-run('insert', 4, 'user_id', 'username', 'active', 'rate', 4, 'user4', 'true', 1.6)
-run('insert', 4, 'user_id', 'username', 'active', 'rate', 5, 'user5', 'false', 0.3)
-run('insert', 4, 'user_id', 'username', 'active', 'rate', 6, 'user6', 'true', 2.9)
+scripts['swap'](args=[str(uuid4())])
 
-run('swap', 0, str(uuid4()))
-
-data = json.loads(run('export', 1, 'all', 'test'))
+data = json.loads(scripts['export'](keys=['all'], args=['test']))
 c.insert('test', data=list(data['data'].values()), column_names=list(data['data'].keys()), column_oriented=True)
 
-run('clean')
-
+scripts['clean']()
 print(c.query("SELECT * FROM test").result_rows)
