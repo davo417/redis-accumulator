6c50276ae2906a521bc5bf9a4f32341b0a4e00d2
davidcode43w <davidgutierrezmenendez@gmail.com>
2023-01-29 19:37:43 -0500

    clean chunk data

---
+++ src/clean.lua
@@ -0,0 +1,23 @@
+-- clean.lua
+-- Delete old chunk data
+
+-- e.g.: redis-cli --eval clean.lua
+
+
+-- Swaped chunk id
+local id = redis.call("get", "chunk:id:swap");
+
+-- Exis if no swaped chunk is set
+if not id then
+    return nil;
+end
+
+-- Delete swaped chunk lenght
+redis.call("del", "chunk:"..id..":len");
+
+-- Delete swaped chunk fields
+local fields = redis.call("keys", "chunk:"..id..":field:*");
+redis.call("del", unpack(fields));
+
+-- Delete swaped chunk id
+return redis.call("del", "chunk:id:swap");
