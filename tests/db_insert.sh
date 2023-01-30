b964eb4c2895ad9ea7f4e7eed1c9abede732fdca
davidcode43w <davidgutierrezmenendez@gmail.com>
2023-01-29 22:52:50 -0500

    create lua folder

--- tests/db_insert.sh
+++ tests/db_insert.sh
@@ -2,6 +2,8 @@
 
 echo "START"
 
+LUA_HOME='./src/lua'
+
 # CLICKHOUSE DB
 clickhouse-client --query="DROP DATABASE IF EXISTS redis" > /dev/null
 clickhouse-client --query="CREATE DATABASE IF NOT EXISTS redis" > /dev/null
@@ -11,31 +13,31 @@ clickhouse-client --query="""CREATE TABLE redis.test (user_id UInt32, username S
 redis-cli flushall > /dev/null
 
 # set matching schema
-redis-cli --eval ../src/schema.lua 'test' 'user_id' 'username' 'active' 'rate' , 'UInt32' 'String' 'Bool' 'Float64' > /dev/null
+redis-cli --eval $LUA_HOME/schema.lua 'test' 'user_id' 'username' 'active' 'rate' , 'UInt32' 'String' 'Bool' 'Float64' > /dev/null
 
 # set chunk id
-redis-cli --eval ../src/swap.lua , $(uuidgen -r) > /dev/null
+redis-cli --eval $LUA_HOME/swap.lua , $(uuidgen -r) > /dev/null
 # add records
-redis-cli --eval ../src/insert.lua 'user_id' 'username' 'active' 'rate' , 1 'user1' false 5.0> /dev/null
-redis-cli --eval ../src/insert.lua 'user_id' 'username' 'active' 'rate' , 2 'user2' true  4.5> /dev/null
+redis-cli --eval $LUA_HOME/insert.lua 'user_id' 'username' 'active' 'rate' , 1 'user1' false 5.0> /dev/null
+redis-cli --eval $LUA_HOME/insert.lua 'user_id' 'username' 'active' 'rate' , 2 'user2' true  4.5> /dev/null
 # swap chunk
-redis-cli --eval ../src/swap.lua , $(uuidgen -r) > /dev/null
+redis-cli --eval $LUA_HOME/swap.lua , $(uuidgen -r) > /dev/null
 # export previous chunk with test schema
-redis-cli --eval ../src/export.lua 'user_id' 'username' 'active' 'rate' , 'test' | clickhouse-client --query="INSERT INTO redis.test FORMAT JSONColumnsWithMetadata"
+redis-cli --eval $LUA_HOME/export.lua 'user_id' 'username' 'active' 'rate' , 'test' | clickhouse-client --query="INSERT INTO redis.test FORMAT JSONColumnsWithMetadata"
 # clean previous chunk data
-redis-cli --eval ../src/clean.lua > /dev/null
+redis-cli --eval $LUA_HOME/clean.lua > /dev/null
 
 # add records
-redis-cli --eval ../src/insert.lua 'user_id' 'username' 'active' 'rate' , 3 'user3' true  3.1> /dev/null
-redis-cli --eval ../src/insert.lua 'user_id' 'username' 'active' 'rate' , 4 'user4' false 1.6> /dev/null
-redis-cli --eval ../src/insert.lua 'user_id' 'username' 'active' 'rate' , 5 'user5' true  0.3> /dev/null
-redis-cli --eval ../src/insert.lua 'user_id' 'username' 'active' 'rate' , 6 'user6' false 2.9> /dev/null
+redis-cli --eval $LUA_HOME/insert.lua 'user_id' 'username' 'active' 'rate' , 3 'user3' true  3.1> /dev/null
+redis-cli --eval $LUA_HOME/insert.lua 'user_id' 'username' 'active' 'rate' , 4 'user4' false 1.6> /dev/null
+redis-cli --eval $LUA_HOME/insert.lua 'user_id' 'username' 'active' 'rate' , 5 'user5' true  0.3> /dev/null
+redis-cli --eval $LUA_HOME/insert.lua 'user_id' 'username' 'active' 'rate' , 6 'user6' false 2.9> /dev/null
 # swap chunk
-redis-cli --eval ../src/swap.lua , $(uuidgen -r) > /dev/null
+redis-cli --eval $LUA_HOME/swap.lua , $(uuidgen -r) > /dev/null
 # export previous chunk with test schema
-redis-cli --eval ../src/export.lua 'all' , 'test' | clickhouse-client --query="INSERT INTO redis.test FORMAT JSONColumnsWithMetadata"
+redis-cli --eval $LUA_HOME/export.lua 'all' , 'test' | clickhouse-client --query="INSERT INTO redis.test FORMAT JSONColumnsWithMetadata"
 # clean previous chunk data
-redis-cli --eval ../src/clean.lua > /dev/null
+redis-cli --eval $LUA_HOME/clean.lua > /dev/null
 
 clickhouse-client --query="SELECT * FROM redis.test"
 
--- tests/redis_flow.sh
+++ tests/redis_flow.sh
@@ -7,44 +7,46 @@ redis-cli flushall > /dev/null
 echo "FLUSH"
 echo
 
+LUA_HOME='./src/lua'
+
 # set test schema
-redis-cli --eval ../src/schema.lua 'test' 'a' 'b' 'c' 'd' , 'Int32' 'Float64' 'Bool' 'String' > /dev/null
+redis-cli --eval $LUA_HOME/schema.lua 'test' 'a' 'b' 'c' 'd' , 'Int32' 'Float64' 'Bool' 'String' > /dev/null
 echo "Schema:"
 echo "$(redis-cli hgetall schema:test)"
 echo
 
 # set chunk id
-redis-cli --eval ../src/swap.lua , 123 > /dev/null
+redis-cli --eval $LUA_HOME/swap.lua , 123 > /dev/null
 # add records
-redis-cli --eval ../src/insert.lua 'a' 'b' 'c' 'd' , 1  2.1    false   'hello' > /dev/null
-redis-cli --eval ../src/insert.lua 'a' 'b' 'c' 'd' , 3  1.33   true    'world' > /dev/null
+redis-cli --eval $LUA_HOME/insert.lua 'a' 'b' 'c' 'd' , 1  2.1    false   'hello' > /dev/null
+redis-cli --eval $LUA_HOME/insert.lua 'a' 'b' 'c' 'd' , 3  1.33   true    'world' > /dev/null
 # check chunk length
 echo "Lenght: $(redis-cli get chunk:123:len)"
 echo
 
 # swap chunk
-redis-cli --eval ../src/swap.lua , 456 > /dev/null
+redis-cli --eval $LUA_HOME/swap.lua , 456 > /dev/null
 # export previous chunk with test schema
-echo "$(redis-cli --eval ../src/export.lua 'a' 'b' 'c' 'd' , 'test')" | python -m json.tool
+echo "$(redis-cli --eval $LUA_HOME/export.lua 'a' 'b' 'c' 'd' , 'test')" | python -m json.tool
 # clean previous chunk data
-redis-cli --eval ../src/clean.lua > /dev/null
+redis-cli --eval $LUA_HOME/clean.lua > /dev/null
 echo
 
 # add records
-redis-cli --eval ../src/insert.lua 'a' 'b' 'c' 'd' , 5  0.5     true    'test'          > /dev/null
-redis-cli --eval ../src/insert.lua 'a' 'b' 'c' 'd' , 10 4.12    false   'of'            > /dev/null
-redis-cli --eval ../src/insert.lua 'a' 'b' 'c' 'd' , 8  8.333   true    'redis'         > /dev/null
-redis-cli --eval ../src/insert.lua 'a' 'b' 'c' 'd' , 1  6.1     false   'accumulator'   > /dev/null
+redis-cli --eval $LUA_HOME/insert.lua 'a' 'b' 'c' 'd' , 5  0.5     true    'test'          > /dev/null
+redis-cli --eval $LUA_HOME/insert.lua 'a' 'b' 'c' 'd' , 10 4.12    false   'of'            > /dev/null
+redis-cli --eval $LUA_HOME/insert.lua 'a' 'b' 'c' 'd' , 8  8.333   true    'redis'         > /dev/null
+redis-cli --eval $LUA_HOME/insert.lua 'a' 'b' 'c' 'd' , 1  6.1     false   'accumulator'   > /dev/null
 # check chunk length
 echo "Lenght: $(redis-cli get chunk:456:len)"
 echo
 
 # swap chunk
-redis-cli --eval ../src/swap.lua , 789 > /dev/null
+redis-cli --eval $LUA_HOME/swap.lua , 789 > /dev/null
 # export previous chunk with test schema
-echo "$(redis-cli --eval ../src/export.lua 'all' , 'test')" | python -m json.tool
+echo "$(redis-cli --eval $LUA_HOME/export.lua 'all' , 'test')" | python -m json.tool
 # clean previous chunk data
-redis-cli --eval ../src/clean.lua > /dev/null
+redis-cli --eval $LUA_HOME/clean.lua > /dev/null
 echo
 
 echo "END"
