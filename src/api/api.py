a43cea9ab27fbb98d998c95914480c143a5e0f17
davidcode43w <davidgutierrezmenendez@gmail.com>
2023-01-31 01:12:22 -0500

    swap script and endpoint

--- src/api/api.py
+++ src/api/api.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from fastapi import status
 from pathlib import Path
 from glob import glob
+import asyncio
 
 
 app = FastAPI()
@@ -25,34 +26,47 @@ class Test(BaseModel):
     active: bool
     rate: float
     
-class Flush(BaseModel):
+class Rows(BaseModel):
     row_count: int
 
 
-async def do_flush() -> int: 
-    for chunk in (await r.smembers("chunk:id:swap")):
-        insert = await subprocess.create_subprocess_shell(
-            f'echo $(redis-cli --eval ../lua/export.lua "{chunk}" "test" , "all")' + ' | ' +
-            'clickhouse-client --query="INSERT INTO redis.test FORMAT JSONColumnsWithMetadata"'
+async def do_export(chunk_id) -> bytes | None:
+    insert = await subprocess.create_subprocess_shell(
+        f'echo $(redis-cli --eval ../lua/export.lua "{chunk_id}" "test" , "all")' + ' | ' +
+        'clickhouse-client --query="INSERT INTO redis.test FORMAT JSONColumnsWithMetadata"'
+    )
+    ierr, iout = await insert.communicate()
+    if (ierr is not None) and (len(ierr) != 0):
+        return ierr
+
+    await r.smove("chunk:id:swap", "chunk:id:clean", chunk_id)
+
+async def do_flush() -> int:
+    inserts = await asyncio.gather(*[do_export(chunk) for chunk in await r.smembers("chunk:id:swap")])
+    inserts = list(filter(lambda ierr: ierr is not None, inserts))
+    
+    if len(inserts) > 0:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={'messages': inserts}
         )
-        ierr, iout = await insert.communicate()
-        if (ierr is not None) and (len(ierr) != 0):
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail={'message': ierr}
-            )
-        await r.smove("chunk:id:swap", "chunk:id:clean", chunk)
 
     return int(await scripts['clean'](keys=['all']))
 
 
 @app.post("/create/", status_code=status.HTTP_201_CREATED)
-async def create(test: Test) -> int:
+async def create(test: Test) -> Rows:
     test_data: dict = test.dict()
-    test_data['active'] = str(test_data['active']).lower()    
-    return await scripts['insert'](keys=list(test_data.keys()), args=list(test_data.values()))
+    test_data['active'] = str(test_data['active']).lower()
+    length = await scripts['insert'](keys=list(test_data.keys()), args=list(test_data.values()))
+    return Rows(row_count=int(length)) 
+
+@app.get("/swap/")
+async def swap() -> Rows:
+    length = await scripts['swap']()
+    return Rows(row_count=int(length))
 
 @app.get("/flush/")
-async def flush() -> Flush:
+async def flush() -> Rows:
     inserted = await do_flush()
-    return Flush(row_count=inserted)
+    return Rows(row_count=inserted)
--- src/lua/swap.lua
+++ src/lua/swap.lua
@@ -7,19 +7,27 @@
 -- Current chunk id
 local id = redis.call("get", "chunk:id:current");
 
+-- Swaped chunk length
+local length = 0;
+
 -- Swap chunk id for export if current is set
 if id then
     redis.call("sadd", "chunk:id:swap", id);
+
+    -- Get swaped chunk length
+    length = redis.call("get", "chunk:"..id..":len");
 
     -- Increment current chunk id
     id = redis.call("incr", "chunk:id:current");
     
     -- Set new chunk length to 0
-    return redis.call("set", "chunk:"..id..":len", 0);
+    redis.call("set", "chunk:"..id..":len", 0);
 else
     -- Set new chunk id
     redis.call("set", "chunk:id:current", 0);
 
     -- Set new chunk length to 0
-    return redis.call("set", "chunk:0:len", 0);
+    redis.call("set", "chunk:0:len", 0);
 end
+
+return length;
